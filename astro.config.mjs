df65772a39fd09e3ca7540c60755fc4c443520c5
Takaya Oshima <oshima-t@ny-tokyo.com>
2024-05-09 15:27:39 +0900

    feat:

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://663c34f0b53863404772da55--wonderful-fairy-418650.netlify.app"
+    site: "https://example.com",
+    integrations: [preact()]
 });
