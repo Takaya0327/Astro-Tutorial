8c5d7519df5c1e982f35cd164fa25a6d289d6b49
Takaya Oshima <oshima-t@ny-tokyo.com>
2024-05-09 15:07:26 +0900

    feat:

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://663c34f0b53863404772da55--wonderful-fairy-418650.netlify.app"
+});
